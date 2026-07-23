8cc91b0663293960a5aeccf3a9f9ba42f7979a1c
AndersonnTech <anderson2csilva98@gmail.com>
2026-07-23 11:05:17 -0300

    Aula 6: Criando Entidades de Persistência, separação entre entidades de dominio e entidades de persistencia, criação da interface que extende de CrudRepository para realizar persistencia em DB, e também a classe JpaProposalRepository que implementa as regras de negocio para Repository

---
+++ src/main/java/project/proposalmanagement/proposal/infrastructure/persistence/entity/ProposalEntity.java
@@ -0,0 +1,55 @@
+package project.proposalmanagement.proposal.infrastructure.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import project.proposalmanagement.proposal.domain.Owner;
+import project.proposalmanagement.proposal.domain.OwnerId;
+import project.proposalmanagement.proposal.domain.Proposal;
+import project.proposalmanagement.proposal.domain.ProposalId;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProposalEntity {
+    @Id
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private UUID ownerId;
+
+    @Column(nullable = false)
+    private String ownerName;
+
+    public static ProposalEntity from(Proposal proposal){
+        return new ProposalEntity(
+                proposal.getId().id(),
+                proposal.getTitle(),
+                proposal.getDescription().orElse(null),
+                proposal.getOwner().id().id(),
+                proposal.getOwner().name()
+        );
+    }
+
+    public Proposal toDomain(){
+        return new Proposal(
+                new ProposalId(this.id),
+                this.title,
+                Optional.ofNullable(this.description),
+                new Owner(new OwnerId(this.ownerId), this.ownerName)
+        );
+    }
+
+}
